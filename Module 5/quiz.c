2191ad34814413eec9f9582a0c76868e46473972
Arif Hossan <arif.hossan.eu@gmail.com>
2025-04-16 00:24:35 +0600

    some random problem in c

--- Module 5/quiz.c
+++ Module 5/quiz.c
@@ -1,1 +1,140 @@
+// #include <stdio.h>
+// int main()
+// {
+//     int a, b, c;
+//     scanf("%d %d %d", &a,&b,&c);
+//     if (a > b && b > c)
+//     {
+//         printf("%d", a);
+//     }
+//     else if (b > a && c > b)
+
+//     {
+//         printf("%d", b);
+//     }
+//     else
+//     {
+//         printf("%d", c);
+//     }
+//     return 0;
+// }
+
+/*
 #include <stdio.h>
+int main ()
+{
+    int x=1;
+    if(x<5)
+    {
+        printf("hello");
+    }
+    if(x<=10)
+    {
+        printf("hi");
+    }
+    else{
+        printf("no");
+    }
+    return 0;
+}
+*/
+
+/*
+#include <stdio.h>
+int main ()
+{
+    int x;
+    scanf("%d",&x);
+    if(x%2!=0)
+    {
+        printf("Odd");
+    }
+    else {
+        printf("Even");
+    }
+    if(x>0)
+    {
+        printf("Positive");
+    }
+    else{
+        printf("Negative");
+    }
+    return 0;
+}
+*/
+/*
+#include <stdio.h>
+int main ()
+{
+    int x=1;
+    if(x<5)
+    {
+        printf("hello");
+    }
+    if(x<=10)
+    {
+        printf("hi");
+    }
+    else{
+        printf("no");
+    }
+    return 0;
+}
+*/
+/*
+#include <stdio.h>
+int main ()
+{
+    int marks=76;
+    if(marks>=80)
+    {
+        printf("A+");
+    }
+    else if(marks>=70)
+    {
+        printf("A");
+    }
+    else if(marks>=70){
+        printf("A-");
+    }
+    return 0;
+}
+    */
+/*
+#include <stdio.h>
+int main ()
+{
+    int num=5;
+    if(num>10)
+    {
+        printf("Greater than 10");
+    }
+    else if(num<10)
+    {
+        printf("Less than 10");
+    }
+    else{
+        printf("Equal to zero");
+    }
+    return 0;
+}
+*/
+
+#include <stdio.h>
+int main()
+{
+    int x = 0;
+    if (x == 1)
+    {
+        if (x >= 0)
+        {
+            printf("true\n");
+        }
+        else
+        {
+            printf("false\n");
+        }
+    }
+
+    return 0;
+}
