e42a4d8e58288a3e32b7f74e56e3d31dddbaceca
Arif Hossan <arif.hossan.eu@gmail.com>
2025-04-16 23:34:23 +0600

    print even numbers to the given input

--- Module 6/tempCodeRunnerFile.c
+++ Module 6/tempCodeRunnerFile.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
 int main ()
 {
-    int x=100;
-    int y = ++x;
-    printf("%d %d",x,y);
+    int x;
+    scanf("%d",&x);
+    for(int i=1;i<=x;i++){
+        if(i%2==0){
+            printf("%d\n",i);
+        }
+    }
     return 0;
 }
