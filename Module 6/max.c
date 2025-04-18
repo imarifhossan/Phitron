6aeaa430b8cd287aa0c1d6b02405dc5b263f759c
Arif Hossan <arif.hossan.eu@gmail.com>
2025-04-18 15:50:01 +0600

    Find the max value when the input in less than 0

--- Module 6/max.c
+++ Module 6/max.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+/*#include <stdio.h>
 int main()
 {
     int n;
@@ -14,3 +14,25 @@ int main()
     printf("%d",max);
     return 0;
 }
+
+*/
+
+/*
+#include <stdio.h>
+#include <limits.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int max=Int_MIN; //it returns the lowest value of integer
+    for (int i=1;i<=n;i++){
+        int val;
+        scanf("%d",&val);
+        if(val>max){
+            max=val;
+        }
+    }
+    printf("%d",max);
+    return 0;
+}
+*/
