b4b1f98e631ceb5752bc3e0e2d88acf9199b0eb2
Arif Hossan <arif.hossan.eu@gmail.com>
2025-04-22 01:37:06 +0600

    array input and output in c

---
+++ Module 7/array_input_and_output.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main(){
+    int arr[5];
+    /*
+    // manual way of getting input 
+    scanf("%d %d %d %d %d",&arr[0],&arr[1],&arr[2],&arr[3],&arr[4]);
+    printf("%d %d %d %d %d",arr[0],arr[1],arr[2],arr[3],arr[4]);
+    */
+
+   /*(int i=0;i<5;i++)*/
+   /*(int i=0;i<n+1;i++)*/
+   /*(int i=0;i<=n-1;i++)*/
+   for (int i=0;i<=4;i++){
+    scanf("%d",&arr[i]);
+   }
+   for (int i=0;i<=4;i++){
+    printf("%d ",arr[i]);
+   }
+    return 0;
+}
