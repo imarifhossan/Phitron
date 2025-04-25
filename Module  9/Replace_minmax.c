26de6ccb4367017477b310a62835314924593581
Arif Hossan <arif.hossan.eu@gmail.com>
2025-04-25 14:45:26 +0600

    reversing an array

---
+++ Module  9/Replace_minmax.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int a[n];
+    for(int i=0;i<n;i++){
+        scanf("%d",&a[i]);
+    }
+    // find the minimum number of the array
+    int min=a[0],min_index=0;
+    for (int i=0;i<n;i++){
+        if(a[i]<min){
+            min=a[i];
+            min_index=i;
+        }
+    }
+    int max=a[0],max_index=0;
+    for (int i=0;i<n;i++){
+        if(a[i]>max){
+            max=a[i];
+            max_index=i;
+        }
+    }
+    int temp=min;
+    
+
+    printf("%d %d     %d %d",min ,min_index ,max, max_index);
+    // int max=
+    // int i=0,j=n-1;
+    // while(i<j){
+    //     int min=a[i];
+
+    // }
+    return 0;
+}
