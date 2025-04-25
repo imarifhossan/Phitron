a40cf0f2314541acf8a306caffedc45b359e56bf
Arif Hossan <arif.hossan.eu@gmail.com>
2025-04-26 00:36:13 +0600

    string input output in c

---
+++ Module 10/string.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int main()
+{
+    char s[10];
+    scanf("%s",s);
+    printf("%s",s);
+    return 0;
+}
+
+
+
+/*
+#include <stdio.h>
+int main()
+{
+    char a[4];
+    for(int i=0;i<4;i++){
+        scanf("%c",&a[i]);
+    }
+    for(int i=0;i<4;i++){
+        printf("%c\n",a[i]);
+    }
+    return 0;
+}
+*/
