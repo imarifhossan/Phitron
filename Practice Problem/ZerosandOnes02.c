40abd61ad128efb1a70bbc398325a5f3964d2957
Arif Hossan <arif.hossan.eu@gmail.com>
2025-04-29 00:37:50 +0600

    zero or one problem in c

---
+++ Practice Problem/ZerosandOnes02.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    int a[n];
+    for (int i = 1; i <=n; i++)
+    {
+        scanf("%d", &a[i]);
+    }
+    int x;
+    scanf("%d", &x);
+    int value = a[x];
+    if (value == 0)
+    {
+        a[x] = 1;
+    }
+    else
+    {
+        a[x] = 0;
+    }
+    for (int i = 1; i <= n; i++)
+    {
+        printf("%d ", a[i]);
+    }
+    return 0;
+}
