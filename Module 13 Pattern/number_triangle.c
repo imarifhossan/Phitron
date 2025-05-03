a21e63fba31bf6413f07cc3fab96896556e982cf
Arif Hossan <arif.hossan.eu@gmail.com>
2025-05-04 05:39:06 +0600

    number triagle pattern in c

---
+++ Module 13 Pattern/number_triangle.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+int main ()
+{
+    int n;
+    scanf("%d",&n);
+    int star = 1;
+    int space = n-1;
+    for(int i=1;i<=n;i++) //for printing line
+    {
+        for(int j=1;j<=space;j++)//for printing space
+        {
+           printf(" ");
+        }
+        for (int k=1;k<=star;k++) //for printing star
+        {
+            printf("%d",k);
+        }
+        printf("\n"); 
+        star++;
+        space--;
+    }
+    return 0;
+}
+
+/*
+    1
+   12
+  123
+ 1234
+12345
+*/
