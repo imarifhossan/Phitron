79ba9d2bff2228f76f7598ff92047cbf1b18bd5d
Arif Hossan <arif.hossan.eu@gmail.com>
2025-05-08 02:44:24 +0600

    no return no parameter in c

--- Module 14 Functions/noreturnnoparameter.c
+++ Module 14 Functions/noreturnnoparameter.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 // as this function does not return anything we have to declare here void
-void sum (int a, int b){
-    
+void sum (){
+    int a,b;
+    scanf("%d %d",&a,&b);
     int sum =  a+b;
     // return;
     printf("%d",sum);
 }
 int main (){
-     int a,b;
-     scanf("%d %d",&a,&b);
-     sum(a,b);
+     
+     sum();
     
     return 0;
 }
