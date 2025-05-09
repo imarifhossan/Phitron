04d83ec4ca1658e1a9c1edd9ddfaae0630e0ae41
Arif Hossan <arif.hossan.eu@gmail.com>
2025-05-09 19:34:18 +0600

    functions in c

---
+++ Module 14 Functions/functions.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <math.h>
+int main (){
+    int a = ceil(5.2);
+    printf("%d\n",a);
+
+    int a2 = floor(5.2);
+    printf("%d\n",a2);
+
+    int a3 = round(5.2);
+    printf("%d\n",a3);
+
+    double a4 = sqrt(5.2);
+    printf("%llf\n",a4);
+
+    int a5 = pow(4,2);
+    printf("%d\n",a5);
+}
