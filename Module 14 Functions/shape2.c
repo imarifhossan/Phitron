90d3d365f27d801ff2c1ca5ab66fc8953ffd9904
Arif Hossan <arif.hossan.eu@gmail.com>
2025-05-09 22:11:20 +0600

    Triangle in c

---
+++ Module 14 Functions/shape2.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    int star=1;
+    int space=n-1;
+    for (int i = 1; i <= n; i++)
+    {
+        for(int i=1;i<=space;i++){
+            printf(" ");
+        }
+        for (int j = 1; j <= star*2-1; j++)
+        {
+            printf("*");
+            
+        }
+        
+        printf("\n");
+        star++;
+        space--;
+    }
+
+    return 0;
+}
