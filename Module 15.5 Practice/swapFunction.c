5cbf9cdeca3515e7feb7e3aea691266176319171
Arif Hossan <arif.hossan.eu@gmail.com>
2025-05-14 02:14:41 +0600

    swap function using change the value of two number postion in c

---
+++ Module 15.5 Practice/swapFunction.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+void swap_it(int* a, int* b){
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+    printf("%d %d",*a,*b);
+}
+int main (){
+    int a,b;
+    scanf("%d %d",&a, &b);
+    swap_it(&a,&b);
+}
