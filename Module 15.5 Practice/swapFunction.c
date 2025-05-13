20e357cd6af15282a07ce205b4d224b5838115f3
Arif Hossan <arif.hossan.eu@gmail.com>
2025-05-14 02:16:16 +0600

    print from the main function in c

--- Module 15.5 Practice/swapFunction.c
+++ Module 15.5 Practice/swapFunction.c
@@ -3,10 +3,11 @@ void swap_it(int* a, int* b){
     int temp = *a;
     *a = *b;
     *b = temp;
-    printf("%d %d",*a,*b);
 }
 int main (){
     int a,b;
     scanf("%d %d",&a, &b);
     swap_it(&a,&b);
+    
+    printf("%d %d",a,b);
 }
