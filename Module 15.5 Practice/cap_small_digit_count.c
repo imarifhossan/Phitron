f0e7c4b167a4b0851a43e7a8386ff694b8300108
Arif Hossan <arif.hossan.eu@gmail.com>
2025-05-16 22:21:46 +0600

    Capital, small and digit count from the string using function in c

--- Module 15.5 Practice/cap_small_digit_count.c
+++ Module 15.5 Practice/cap_small_digit_count.c
@@ -3,13 +3,21 @@
 
 void DigitCount (char a[]){
     int len = strlen(a);
-    int count = 0;
+    int count = 0, cap_count=0,small_count=0;
     for(int i=0;i<len;i++){
         if(a[i]>='0'  && a[i]<='9'){
             count++;
         }
+        if(a[i]>='a'  && a[i]<='z'){
+            small_count++;
+        }
+        if(a[i]>='A'  && a[i]<='Z'){
+            cap_count++;
+        }
     }
-    printf("%d",count);
+    printf("Digit count = %d\n",count);
+    printf("Capital count = %d\n",cap_count);
+    printf("Small count = %d",small_count);
 }
 int main (){
     char s[100001];
