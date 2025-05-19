7661600f626a41c330877b7d7e15d880480957fb
Arif Hossan <arif.hossan.eu@gmail.com>
2025-05-20 03:58:47 +0600

    scaler matrix check

--- Module 18 2D Array/scaler_matrix.c
+++ Module 18 2D Array/scaler_matrix.c
@@ -1,54 +1,3 @@
-// #include <stdio.h>
-// #include <stdbool.h>
-// int main()
-// {
-//     int r, c;
-//     scanf("%d %d", &r, &c);
-//     int a[r][c];
-//     for (int i = 0; i < r; i++)
-//     {
-//         for (int j = 0; j < c; j++)
-//         {
-//             scanf("%d", &a[i][j]);
-//         }
-//     }
-//     bool is_diagonal = true;
-
-//     if (r == c)
-//     {
-
-//         for (int i = 0; i < r; i++)
-//         {
-//             for (int j = 0; j < c; j++)
-//             {
-//                 if (i +j !=r-1)
-                
-                
-//                 {
-//                     //we are now at outside of secondary diagonal 
-//                     if (a[i][j] != 0)
-//                     {
-//                         is_diagonal=false;
-//                         printf("This is not a secondary diagonal matrix");
-//                     }
-//                 }
-//             }
-//         }
-//         if(is_diagonal==true){
-//             printf("This is a secondary diagonal matrix");
-//         }
-//     }
-//     else
-//     {
-//         printf("Its not a secondary diagonal matrix");
-//     }
-
-//     return 0;
-// }
-
-
-
-
 #include <stdio.h>
 #include <stdbool.h>
 int main()
@@ -63,9 +12,10 @@ int main()
             scanf("%d", &a[i][j]);
         }
     }
-    // bool is_diagonal = true;
+    bool is_diagonal = true;
     bool is_scaler = true;
     int temp = a[0][0];
+
     if (r == c)
     {
 
@@ -73,20 +23,38 @@ int main()
         {
             for (int j = 0; j < c; j++)
             {
-               
+                if (i == j)
+                {
+                    // diagonal position
+                    if(temp != a[i][j]){
+                        is_scaler=false;
+                    }
+                }
+                else
+                {
+                    if (a[i][j] != 0)
+                    {
+                        is_diagonal=false;
+                    }
+                }
             }
         }
-        
-
-         if(is_scaler==true){
-                printf("This is a scaler matrix");
-            }
+        if(is_diagonal==true && is_scaler==true){
+            printf("This is a scaler matrix");
+        }
+        else if (is_diagonal == true && is_scaler == false)
+        {
+            printf("This is a diagonal matrix but not a scaler matrix\n");
+        }
         else
+        {
+            printf("This is not a scaler matrix\n");
+        }
+    }
+    else
     {
-        printf("Its not a scaler matrix line 105");
+        printf("Its not a scaler matrix");
     }
-    }
-    
 
     return 0;
 }
@@ -95,6 +63,6 @@ int main()
 /*
 3 3
 5 0 0
-0 5 0
-0 0 5
+0 6 0
+0 0 1
 */
