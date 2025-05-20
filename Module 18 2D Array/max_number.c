8361aecb6786e58b0a537bdadc6f7a3cf918d6f3
Arif Hossan <arif.hossan.eu@gmail.com>
2025-05-21 04:05:11 +0600

    find max number from array

--- Module 18 2D Array/max_number.c
+++ Module 18 2D Array/max_number.c
@@ -1,4 +1,32 @@
 #include <stdio.h>
+int maxNumber (int a[],int n,int i){
+    if(i==n-1){
+        return a[i];
+    }
+    
+    
+    int maxValue = maxNumber(a,n,i+1);
+    if(a[i]>maxValue){
+        return a[i];
+    }
+    else  {
+        return maxValue;
+    }
+
+}
+int main (){
+    int n;
+    scanf("%d",&n);
+    int a[n];
+    for(int i=0;i<n;i++){
+        scanf("%d",&a[i]);
+    }
+    int answer = maxNumber(a,n,0);
+    printf("%d",answer);
+    return 0;
+}
+/*
+#include <stdio.h>
 void maxNumber (int a[],int n,int i,int max){
     if(i==n){
         printf("%d",max);
@@ -21,3 +49,4 @@ int main (){
     maxNumber(a,n,0,a[0]);
     return 0;
 }
+*/
