c2eea75e606f35048c41fd8a379aabe35210d6ef
Arif Hossan <arif.hossan.eu@gmail.com>
2025-05-21 04:20:09 +0600

    cd

--- Module 18 2D Array/summation.c
+++ Module 18 2D Array/summation.c
@@ -1,4 +1,27 @@
 #include <stdio.h>
+long long int sumOfArray (int a[],int n,int i){
+    if(i==n-1){
+        return a[i];
+    }
+    
+    
+    long long int sum = sumOfArray(a,n,i+1);
+    return a[i]+sum;
+
+}
+int main (){
+    int n;
+    scanf("%d",&n);
+    int a[n];
+    for(int i=0;i<n;i++){
+        scanf("%d",&a[i]);
+    }
+    long long int answer = sumOfArray(a,n,0);
+    printf("%lld",answer);
+    return 0;
+}
+
+/*#include <stdio.h>
 void sumofArray(int a[],int n,int i,long long int sum){
 if(i==n){
     printf("%lld",sum);
@@ -16,4 +39,4 @@ int main (){
     long long int sum = 0;
     sumofArray(a,n,0,sum);
 
-}
+}*/
