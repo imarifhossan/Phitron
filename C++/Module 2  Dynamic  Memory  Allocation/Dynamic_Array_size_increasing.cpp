a26989882efff2bf5f4f8bddbbfee25d51fde729
Arif Hossan <arif.hossan.eu@gmail.com>
2025-05-29 23:06:31 +0600

    :memory size increasing in dynamic array in c++

--- C++/Module 2  Dynamic  Memory  Allocation/Dynamic_Array_size_increasing.cpp
+++ C++/Module 2  Dynamic  Memory  Allocation/Dynamic_Array_size_increasing.cpp
@@ -2,6 +2,28 @@
 using namespace std;
 int main ()
 {
-    
+    int n;
+    cin >> n;
+    int *a = new int [n];
+    for(int i=0;i<n;i++){
+        cin >> a[i];
+    }
+    int m;
+    cin >> m;
+    int *b = new int[m];
+    // copy a elements to b array
+    for(int i=0;i<n;i++){
+        b[i]=a[i];
+    }
+    //input remaining element to the b array
+    int remaining = m-n;
+    for (int i=n;i<m;i++){
+        cin >> b[i];
+    }
+    delete[] a;
+    //printing the b array
+    for(int i=0;i<m;i++){
+        cout << b[i] << " ";
+    }
     return 0;
 }
