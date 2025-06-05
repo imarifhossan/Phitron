ad6c65f7780488b5d5985a691504fd53eeb81a36
Arif Hossan <arif.hossan.eu@gmail.com>
2025-06-05 21:23:49 +0600

    String constructor in c++

---
+++ C++/Module 6 Class and String/string.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main (){
+    //General way of taking string input
+    //1
+    // string s = "Hello";
+    //2
+    // string s("Hello");
+    //3
+    //string s("Hello",4);//resize the array  return Hell
+    //4
+    //string s = "Hello world";
+    //string t(s,4);//remove the number from the string and remove rest of it
+    //5
+    string t(5,'A');//create a string size 5 and put A every index
+    cout << t << endl;
+    return 0;
+}
