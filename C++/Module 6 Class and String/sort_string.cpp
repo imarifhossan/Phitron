2233d75eb8cc23befe8b63871eb5a484db55175c
Arif Hossan <arif.hossan.eu@gmail.com>
2025-06-05 21:35:57 +0600

    String sort in c++

---
+++ C++/Module 6 Class and String/sort_string.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main (){
+    string s;
+    cin >> s;//helloworld
+    sort(s.begin(),s.end());//dehllloorw
+    cout << s<< endl;
+    return 0;
+}
